e821c2d59cc5a6a7de81d19d668fb4cf4e5b7a5f
Hugo Lindfors <111429041+hugolindfors@users.noreply.github.com>
2022-11-29 14:34:05 +0100

    Tuesday commit, code is hot mess

--- Program.c
+++ Program.c
@@ -8,155 +8,189 @@
 // Types and Structures Definition
 //----------------------------------------------------------------------------------
 
-typedef struct Tile {
+typedef const enum tile { left, centre_left, centre_right, right };
+typedef const enum tile { up, middle_up, middle_down, down };
+typedef const enum neighbor { up, right, down, left };
 
-    Vector2 position;
-    bool isBlocked;
+typedef struct Tile
+{
+    bool player_can_walk[4];
 } Tile;
 
-typedef struct Node {
-
-    Vector2 position;
+typedef struct Node
+{
     Tile node[3][3];
 } Node;
 
 //----------------------------------------------------------------------------------
 // Global Variable Declaration and Initialization
 //----------------------------------------------------------------------------------
-static const int screenWidth = 2560;
-static const int screenHeight = 1440;
+static const int screenWidth = 800;
+static const int screenHeight = 450;
 
-Node maze[4][4];
+Node maze_block[4][4];
 
 bool generationIsRunning = true;
 
-void GenerateMaze ( void )
-{
+// void GenerateMaze ( void )
+// {
 
-    for ( int i = 0; i < 4; i++ )
+//     for ( int i = 0; i < 4; i++ )
+//     {
+
+//         for ( int j = 0; j < 4; j++)
+//         {
+
+//             for ( int k = 0; k < 3; k++ )
+//             {
+
+//                 for ( int l = 0; l < 3; l++)
+//                 {
+
+//                     // if it's a center-tile, then it's never blocked
+//                     if ( k == 1 && l == 1 )
+//                     {
+//                         maze[i][j].node[k][l].isBlocked = false;
+//                     }
+
+//                     else if
+//                     (
+
+//                         ( ( k == 0 && l == 0 ) || ( k == 2 && l == 2 ) ) || // If tile is corner-tile, then it's always blocked
+//                         ( ( i == 0 || i == 3 ) || ( j == 0 || j == 3 ) ) // If tile is outer-tile, then it's always blocked
+//                     )
+
+//                     {
+
+//                         maze[i][j].node[k][l].isBlocked = true;
+//                     }
+
+//                     else if ( generationIsRunning )
+//                     {
+
+//                         maze[i][j].node[k][l].isBlocked = (rand() % 2);
+//                     }
+
+//                     if ( ( maze[i + 1][j].node[k - 1][l].isBlocked == false ) && ( l == 1 || k == 1 ) )
+//                     {
+//                         maze[i][j].node[k][l].isBlocked = false;
+
+//                         maze[i][j].node[k][l].neighbor = North;
+//                     }
+//                 };
+//             };
+//         };
+//     };
+
+//     generationIsRunning = false;
+// };
+
+void GenerateMaze(void)
+{
+    // for every block
+
+    // check every direction
+
+    // if neigbor is blocked – block direction
+
+    // else – randomize if direction is blocked
+
+    // if you decide to block – save that data
+
+
+    for (int x = left; x < right; x++ ) 
+    {
+        for (int y = up; y < down; y++ )
+        {
+            if (maze_block[left][up].neigbor[up] ==  )
+        }
+    }
+}
+
+void DrawMaze(void)
+{
+    ClearBackground(WHITE);
+
+    Vector2 basePosition = {(float)20, (float)20};
+
+    for (int i = 0; i < 4; i++)
     {
 
-        for ( int j = 0; j < 4; j++)
+        for (int j = 0; j < 4; j++)
         {
 
-            for ( int k = 0; k < 3; k++ )
+            for (int k = 0; k < 3; k++)
             {
 
-                for ( int l = 0; l < 3; l++)
+                for (int l = 0; l < 3; l++)
                 {
-
-                    if ( k == 1 && l == 1 )
+                    if ((maze[i][j].node[k][l].isBlocked == true) && ((k == 0 && l == 0) || (k == 2 && l == 2)) || ((k == 0 && l == 2) || (k == 2 && l == 0)))
                     {
-
-                        maze[i][j].node[k][l].isBlocked = false;
+                        DrawRectangle(100 * k + basePosition.x, 100 * l + basePosition.y, 20, 20, GREEN);
                     }
-
-                    else if 
-                    ( 
-
-                        ( ( k == 0 || k == 2 ) && ( l == 0 || l == 2 ) ) || ( ( ( i == 0 || i == 3 ) && ( j == 0 || j == 3 ) ) && ( k != 1 && l != 1 ) ) // Overly-Complicated Logic
-                    )
-
+                    else if (maze[i][j].node[k][l].isBlocked == true)
                     {
-
-                        maze[i][j].node[k][l].isBlocked = true;
-                    }
-
-                    else if ( generationIsRunning )
-                    {
-
-                        maze[i][j].node[k][l].isBlocked = (rand() % (1 - 0 + 1)) + 0;
-                    }
-                };
-            };
-        };
-    };
-
-    generationIsRunning = false;
-};
-
-void DrawMaze ( void )
-{
-    ClearBackground(BLACK);
-
-    Vector2 basePosition = { (float) 20, (float)  20 };
-
-    for ( int i = 0; i < 4; i++ )
-    {
-
-        for ( int j = 0; j < 4; j++)
-        {
-
-            for ( int k = 0; k < 3; k++ )
-            {
-
-                for ( int l = 0; l < 3; l++)
-                {
-                    if ( ( maze[i][j].node[k][l].isBlocked == true ) && ( ( k == 0 && l == 0 ) || ( k == 2 && l == 2 ) ) || ( ( k == 0 && l == 2 ) || ( k == 2 && l == 0 ) ) ) { DrawRectangle ( 100 * k + basePosition.x, 100 * l + basePosition.y, 20, 20, YELLOW ); }
-                    else if ( maze[i][j].node[k][l].isBlocked == true ) { DrawRectangle ( 100 * k + basePosition.x, 100 * l + basePosition.y, 20, 20, WHITE ); };
+                        DrawRectangle(100 * k + basePosition.x, 100 * l + basePosition.y, 20, 20, BLUE);
+                    };
                 };
             };
 
-            basePosition.x += 200;
+            basePosition.x += 300;
         };
 
         basePosition.x = 20;
 
-        basePosition.y += 200;
+        basePosition.y += 300;
     };
 };
 
-void Main ( void )
+int main(void)
 {
     // Initialization
     //---------------------------------------------------------
-    InitWindow ( screenWidth, screenHeight, "Maze Generate" );
+    InitWindow(screenWidth, screenHeight, "Maze Generate");
 
-    SetTargetFPS ( 60 );
+    SetTargetFPS(60);
     //---------------------------------------------------------
-    
 
-    while ( !WindowShouldClose ( ) )    // Detect Window Close Button or ESC Key
+    while (!WindowShouldClose()) // Detect Window Close Button or ESC Key
     {
+        // Check For Opt + Enter
+        if (IsKeyPressed(KEY_ENTER) && (IsKeyDown(KEY_LEFT_ALT) || IsKeyDown(KEY_RIGHT_ALT)))
+        {
+            // See What Display We Are On Right Now
+            int display = GetCurrentMonitor();
+
+            if (IsWindowFullscreen())
+            {
+                // If we are full screen, then go back to the windowed size
+                SetWindowSize(screenWidth, screenHeight);
+            }
+            else
+            {
+                // If we are not full screen, set the window size to match the monitor we are on
+                SetWindowSize(GetMonitorWidth(display), GetMonitorHeight(display));
+            }
+
+            // toggle the state
+            ToggleFullscreen();
+        }
+
         // Generate
-        GenerateMaze ( );
+        GenerateMaze();
 
         // Draw
-        BeginDrawing ( );
-            DrawMaze ( );
-        EndDrawing ( );
+        BeginDrawing();
+
+        DrawMaze();
+
+        EndDrawing();
     }
-} 
 
+    // De-Initialization
+    //--------------------------------------------------------------------------------------
+    CloseWindow(); // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-int main ( void )
-{
-    Main ();
-    
     return 0;
 };
