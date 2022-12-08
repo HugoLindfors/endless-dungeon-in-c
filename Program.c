8df4823eac22d71c42ece6a57dc537c945da21df
Hugo Lindfors <111429041+hugolindfors@users.noreply.github.com>
2022-12-08 10:35:45 +0100

    Edge init and draw complete!

--- Program.c
+++ Program.c
@@ -4,27 +4,11 @@
 #include <time.h>
 #include <math.h>
 
-//----------------------------------------------------------------------------------
-// Types and Structures Definition
-//----------------------------------------------------------------------------------
+typedef enum Neighbor { up, left, down, right } Neighbor;
 
-typedef enum neighbor
-{
-    up,
-    right,
-    down,
-    left
-} neighbor;
+typedef struct Edge { Vector2 position; float width; float height; Color color; bool is_colored; } Edge;
 
-typedef struct Node
-{
-    Vector2 position;
-    bool is_colored;
-    float width;
-    float height;
-    Color color;
-    bool is_blocked[4];
-} Node;
+typedef struct Node { Vector2 position; float width; float height; Color color; bool is_colored; Edge edge[4]; } Node;
 
 //------------------------------------------------------------------------------------
 // Global Variables Declaration
@@ -32,85 +16,117 @@ typedef struct Node
 static const int screen_width = 800;
 static const int screen_height = 450;
 
+static const edge_long_side = 100;
+static const edge_short_side = 5;
+
 Node maze_block[4][4];
-
-void InitMaze(void) // Initialize maze
-{
-    int row_height = 64, column_width = GetScreenWidth() / 12; // Twelve 'cause why not? (Bootstrap uses 12 columns per row)
-
-    for (int row = 0; row < 4; row++)
-    {
-        for (int column = 0; column < 4; column++)
-        {
-            maze_block[row][column].position.y = row * row_height;
-            maze_block[row][column].position.x = column * column_width;
-
-            maze_block[row][column].width = column_width;
-            maze_block[row][column].height = row_height;
-
-            maze_block[row][column].color = WHITE;
-        }
-    }
-}
-
-void GenerateMaze(void) // Generate maze
-{
-
-    for (int row = 0; row < 4; row++)
-    {
-        for (int column = 0; column < 4; column++)
-        {
-            switch (row)
-            {
-            case 0:
-                maze_block[row][column].is_colored = true;
-                break;
-            
-            case 1:
-                maze_block[row][column].is_colored = true;
-                maze_block[row][column].color = RED;
-                break;
-
-            case 2:
-                maze_block[row][column].is_colored = true;
-                maze_block[row][column].color = GREEN;
-                break;
-
-            case 3:
-                maze_block[row][column].is_colored = true;
-                maze_block[row][column].color = BLUE;
-                break;
-
-            default:
-                maze_block[row][column].is_colored = false;
-                break;
-            }
-        }
-    }
-}
-
-void DrawMaze(void) // Draw maze
-{
-    ClearBackground(BLACK);
-
-    for (int row = 0; row < 4; row++)
-    {
-        for (int column = 0; column < 4; column++)
-        {
-            if (maze_block[row][column].is_colored)
-            {
-                DrawRectangle(maze_block[row][column].position.x, maze_block[row][column].position.y, maze_block[row][column].width, maze_block[row][column].height, maze_block[row][column].color);
-            }
-        }
-    }
-}
 
 //------------------------------------------------------------------------------------
 // Program main entry point
 //------------------------------------------------------------------------------------
+
+void InitMaze(void)
+{
+    for (size_t row = 0; row < 4; row++)
+    {
+        for (size_t col = 0; col < 4; col++) // col(umn)
+        {
+            maze_block[row][col].position.x = col * 100;
+            maze_block[row][col].position.y = row * 100;
+
+            maze_block[row][col].width = 100;
+            maze_block[row][col].height = 100;
+
+            maze_block[row][col].color = RED;
+            maze_block[row][col].is_colored = true;
+
+            for (size_t i_edge = 0; i_edge < 4; i_edge++)
+            {
+                switch (i_edge)
+                {
+                case up:
+                    maze_block[row][col].edge[i_edge].position.x = col * 100;
+                    maze_block[row][col].edge[i_edge].position.y = row * 100;
+
+                    maze_block[row][col].edge[i_edge].width = edge_long_side;
+                    maze_block[row][col].edge[i_edge].height = edge_short_side;
+
+                    maze_block[row][col].edge[i_edge].color = WHITE;
+                    maze_block[row][col].edge[i_edge].is_colored = true;
+                    break;
+
+                case left:
+                    maze_block[row][col].edge[i_edge].position.x = col * 100;
+                    maze_block[row][col].edge[i_edge].position.y = row * 100;
+
+                    maze_block[row][col].edge[i_edge].width = edge_short_side;
+                    maze_block[row][col].edge[i_edge].height = edge_long_side;
+
+                    maze_block[row][col].edge[i_edge].color = WHITE;
+                    maze_block[row][col].edge[i_edge].is_colored = true;
+                    break;
+
+                case down:
+                    maze_block[row][col].edge[i_edge].position.x = col * 100;
+                    maze_block[row][col].edge[i_edge].position.y = row * 100 + 100;
+
+                    maze_block[row][col].edge[i_edge].width = edge_long_side;
+                    maze_block[row][col].edge[i_edge].height = edge_short_side;
+
+                    maze_block[row][col].edge[i_edge].color = WHITE;
+                    maze_block[row][col].edge[i_edge].is_colored = true;
+                    break;
+
+                case right:
+                    maze_block[row][col].edge[i_edge].position.x = col * 100 + 100;
+                    maze_block[row][col].edge[i_edge].position.y = row * 100;
+
+                    maze_block[row][col].edge[i_edge].width = edge_short_side;
+                    maze_block[row][col].edge[i_edge].height = edge_long_side;
+
+                    maze_block[row][col].edge[i_edge].color = WHITE;
+                    maze_block[row][col].edge[i_edge].is_colored = true;
+
+                    // The code above is fine, but it will leave an uncolored square at the bottom right. To compensate the right edge of the lowermost, rightmost maze_block is lenghtened by one edge_short_side
+                    if (row == 3 && col == 3)
+                    {
+                        maze_block[row][col].edge[i_edge].height = edge_long_side + edge_short_side;
+                    }
+
+                    break;
+                }
+            }
+            
+        }  
+    }   
+}
+
+void GenerateMaze(void)
+{
+    
+}
+
+void DrawMaze(void)
+{
+    ClearBackground(BLACK);
+
+    for (size_t row = 0; row < 4; row++)
+    {
+        for (size_t col = 0; col < 4; col++) // col(umn)
+        {
+            DrawRectangle(maze_block[row][col].position.x, maze_block[row][col].position.y, maze_block[row][col].width, maze_block[row][col].height, maze_block[row][col].color);
+
+            for (size_t i_edge = 0; i_edge < 4; i_edge++)
+            {
+                DrawRectangle(maze_block[row][col].edge[i_edge].position.x, maze_block[row][col].edge[i_edge].position.y, maze_block[row][col].edge[i_edge].width, maze_block[row][col].edge[i_edge].height, maze_block[row][col].edge[i_edge].color);
+            }
+            
+        }  
+    }
+}
+
 int main(void)
 {
-    // Initialize
     InitWindow(screen_width, screen_height, "Endless Maze");
 
     InitMaze();
@@ -119,7 +135,7 @@ int main(void)
 
     while (!WindowShouldClose())
     {
-        // Update
+        // Generate
         GenerateMaze();
 
         // Draw
@@ -129,4 +145,4 @@ int main(void)
     }
 
     return 0;
-};
+}
---
+++ program_old.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "raylib.h"
+#include <time.h>
+#include <math.h>
+
+//----------------------------------------------------------------------------------
+// Types and Structures Definition
+//----------------------------------------------------------------------------------
+
+typedef enum neighbor
+{
+    up,
+    right,
+    down,
+    left
+} neighbor;
+
+typedef struct Node
+{
+    Vector2 position;
+    bool is_colored;
+    float width;
+    float height;
+    Color color;
+    bool is_blocked[4];
+} Node;
+
+//------------------------------------------------------------------------------------
+// Global Variables Declaration
+//------------------------------------------------------------------------------------
+static const int screen_width = 800;
+static const int screen_height = 450;
+
+Node maze_block[4][4];
+
+void InitMaze(void) // Initialize maze
+{
+    int row_height = 64, column_width = GetScreenWidth() / 12; // Twelve 'cause why not? (Bootstrap uses 12 columns per row)
+
+    for (int row = 0; row < 4; row++)
+    {
+        for (int column = 0; column < 4; column++)
+        {
+            maze_block[row][column].position.y = row * row_height;
+            maze_block[row][column].position.x = column * column_width;
+
+            maze_block[row][column].width = column_width;
+            maze_block[row][column].height = row_height;
+
+            maze_block[row][column].color = WHITE;
+        }
+    }
+}
+
+void GenerateMaze(void) // Generate maze
+{
+
+    for (int row = 0; row < 4; row++)
+    {
+        for (int column = 0; column < 4; column++)
+        {
+            switch (row)
+            {
+            case 0:
+                maze_block[row][column].is_colored = true;
+                break;
+            
+            case 1:
+                maze_block[row][column].is_colored = true;
+                maze_block[row][column].color = RED;
+                break;
+
+            case 2:
+                maze_block[row][column].is_colored = true;
+                maze_block[row][column].color = GREEN;
+                break;
+
+            case 3:
+                maze_block[row][column].is_colored = true;
+                maze_block[row][column].color = BLUE;
+                break;
+
+            default:
+                maze_block[row][column].is_colored = false;
+                break;
+            }
+        }
+    }
+}
+
+void DrawMaze(void) // Draw maze
+{
+    ClearBackground(BLACK);
+
+    for (int row = 0; row < 4; row++)
+    {
+        for (int column = 0; column < 4; column++)
+        {
+            if (maze_block[row][column].is_colored)
+            {
+                DrawRectangle(maze_block[row][column].position.x, maze_block[row][column].position.y, maze_block[row][column].width, maze_block[row][column].height, maze_block[row][column].color);
+            }
+        }
+    }
+}
+
+//------------------------------------------------------------------------------------
+// Program main entry point
+//------------------------------------------------------------------------------------
+int main(void)
+{
+    // Initialize
+    InitWindow(screen_width, screen_height, "Endless Maze");
+
+    InitMaze();
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+        // Update
+        GenerateMaze();
+
+        // Draw
+        BeginDrawing();
+        DrawMaze();
+        EndDrawing();
+    }
+
+    return 0;
+};
